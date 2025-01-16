7b015cbe873017a40d7ca9d42a60eaa26aaa7788
James Tallman <James Tallman>
2025-01-16 12:44:09 -0600

    fix run_all.py

--- run_all.py
+++ run_all.py
@@ -4,15 +4,14 @@ import os
 import subprocess
 
 
-angles = str(sys.argv[2])
-x_box_size = int(sys.argv[1])
-lipid = str(sys.argv[4])
-i = str(sys.argv[3])
+angle = str(sys.argv[2]) # angle in degrees
+x_box_size = int(sys.argv[1]) #approximate size of the x dimension of the small membrane patch (equilibrated dimension ~ .6 x_box_size)
+lipid = str(sys.argv[4]) #DLPC, DOPC, DPPC, DOPE
+i = str(sys.argv[3]) #run number
 
 
 subprocess.run(['python3', 'init_small.py', str(x_box_size), str(i), lipid])
 subprocess.run(['python3', 'run_small.py', str(x_box_size), str(i), lipid])
-for angle in angles:
-    subprocess.run(['python3', 'init_stitched.py', str(x_box_size), str(angle), str(i), lipid])
-    subprocess.run(['python3', 'fire_stitched.py', str(x_box_size), str(angle), str(i), lipid])
-    subprocess.run(['python3', 'run_stitched.py', str(x_box_size), str(angle), str(i), lipid])
+subprocess.run(['python3', 'init_stitched.py', str(x_box_size), str(angle), str(i), lipid])
+subprocess.run(['python3', 'fire_stitched.py', str(x_box_size), str(angle), str(i), lipid])
+subprocess.run(['python3', 'run_stitched.py', str(x_box_size), str(angle), str(i), lipid])
